29d8662ff453603bbfa6c7a3ceea327a562905d2
Manikandan Anbalagan <manikandana121290@gmail.com>
2026-01-10 12:46:56 +0530

    DOM

---
+++ DOM/index.js
@@ -0,0 +1,7 @@
+const secondLi = document.getElementById("second-li"); //id
+const firstLi = document.querySelector("li"); //cssSelector
+const listItems = document.querySelectorAll("li"); //className
+// secondLi.innerText = "This is the second list item.";
+console.log(secondLi);
+console.log(firstLi);
+console.log(listItems);
